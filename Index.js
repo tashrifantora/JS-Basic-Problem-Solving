dcd642330913274255c7fd08794371db0bc20cfe
Tashrif Antora <125790353+tashrifantora@users.noreply.github.com>
2023-08-02 21:09:19 +0600

    first commit

--- Index.js
+++ Index.js
@@ -1,1 +1,12 @@
 // Problem 1 [Leap Year]
+function leapYear(year) {
+    const remainde = year % 4;
+    if (remainde == 0) {
+        return true;
+    }
+    else {
+        return false
+    }
+}
+const checkYear = leapYear(1958);
+console.log(checkYear);
