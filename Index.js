5da04f1e0a7de79bcbb9b5fb176ca5d4d995a7af
Tashrif Antora <125790353+tashrifantora@users.noreply.github.com>
2023-08-02 21:31:30 +0600

    problem solving 2

--- Index.js
+++ Index.js
@@ -9,4 +9,22 @@ function leapYear(year) {
     }
 }
 const checkYear = leapYear(1958);
-console.log(checkYear);
+// console.log(checkYear);
+
+
+/* 
+Problem 2
+[Write a function findOddSum() that will take the array [5,7,8,10,45,30] as the input para meter and will return the sum of the odd numbers]
+*/
+function getSumOFAnArray(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        const elements = numbers[i];
+        sum = sum + elements
+    }
+    return sum;
+
+}
+const MyNumbers = [25, 98, 45, 63, 21, 74, 55];
+const result = getSumOFAnArray(MyNumbers);
+console.log(result);
